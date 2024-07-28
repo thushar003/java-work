0aead4562b49909b802cf47a8386900926be928a
thushar003 <thusharjoji2@gmail.com>
2024-07-28 17:59:04 -0400

    Create nQueens.java

---
+++ nQueens.java
@@ -0,0 +1,14 @@
+/**
+ * Program Name: nQueens.java
+ * @author: Thushar Joseph Joji
+ * Date: 28 July, 2024
+ * Purpose: To explore the N Queens problem, inspired by LeetCode.
+ */
+
+ public class nQueens {
+    public static void main(String[] args)
+    {
+       
+    }
+ }
+
